dde1676a7f6565f4564d129d2a447dde9bd86f3c
Mohammad Ismail <forwork.ismail@gmail.com>
2022-09-02 14:48:30 +0430

    feat: Solve SumInRange

--- main/SumInRange.kt
+++ main/SumInRange.kt
@@ -1,2 +1,12 @@
 package main
 
+// find sum of integers in range
+val sumInRangeInput = listOf("1", "10", "a", "7", "ad2", "3")
+
+fun sumInRange(input: List<String>, range: IntRange): Int = input
+    .filter { it.matches(Regex("\\d+")) }
+    .map { it.toInt() }
+    .filter { it in range }
+    .sum()
+
+
